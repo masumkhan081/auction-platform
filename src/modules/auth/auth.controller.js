ab08771d5ca19959000e8765c99db973a05d7a16
Masum Khan <masumkhan081@gmail.com>
2024-10-02 21:30:49 +0600

    operable entities , pagination and query wise response hhandler been setup

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -87,45 +87,7 @@ async function sendOTPToEmail(req, res) {
 
   await authService.sendOTPToEmail(email);
 }
-
-async function createUser(req, res) {
-  const result = await addressService.createUser(req.body);
-  if (result instanceof Error) {
-    sendErrorResponse({ res, error: result, what: operableEntities.address });
-  } else {
-    sendCreateResponse({ res, data: result, what: operableEntities.address });
-  }
-}
-
-async function getAddresses(req, res) {
-  const result = await addressService.getAddresses(req.query);
-  if (result instanceof Error) {
-    sendErrorResponse({ res, error: result, what: operableEntities.address });
-  } else {
-    sendFetchResponse({ res, data: result, what: operableEntities.address });
-  }
-}
-//
-async function updateAddress(req, res) {
-  const result = await addressService.updateAddress({
-    id: req.params.id,
-    data: req.body,
-  });
-  if (result instanceof Error) {
-    sendErrorResponse({ res, error: result, what: operableEntities.address });
-  } else {
-    sendUpdateResponse({ res, data: result, what: operableEntities.address });
-  }
-}
-//
-async function deleteAddress(req, res) {
-  const result = await addressService.deleteAddress(req.params.id);
-  if (result instanceof Error) {
-    sendErrorResponse({ res, error: result, what: operableEntities.address });
-  } else {
-    sendDeletionResponse({ res, data: result, what: operableEntities.address });
-  }
-}
+ 
 
 //
 module.exports = {
