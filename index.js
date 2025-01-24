5835ed9ba4094fc241b59a07941a959d2660a66c
masumkhan081 <masumkhan081@gmail.com>
2025-01-25 02:59:04 +0600

    commited

--- index.js
+++ index.js
@@ -1,44 +1,38 @@
-/* eslint-disable no-undef */
-const app = require("./src/app");
-const {initDB,mongodbConnection} = require("./src/config/mongodb");
-const config = require("./src/config");
+const express = require("express");
+const app = express();
+const cors = require("cors");
 require("dotenv").config();
-
-async function bootstrap() {
-  const server = app.listen(config.port, async () => {
-    console.log(`Server running on port ${config.port}`);
-    // await mongodbConnection();
-  });
-
-  const exitHandler = () => {
-    if (server) {
-      server.close(() => {
-        console.log("Server closed");
-      });
-    }
-    process.exit(1);
-  };
-
-  const unexpectedErrorHandler = (error) => {
-    console.log("unexpectedErrorHandler: " + error.message);
-    exitHandler();
-  };
-
-  process.on("uncaughtException", unexpectedErrorHandler);
-  process.on("unhandledRejection", unexpectedErrorHandler);
-}
-// bootstrap();
-
+const { initDB } = require("./src/config/mongodb");
 
 // initialize the database
 initDB();
+
+// middlewares
+const allowedOrigins = ["http://localhost:3001", "http://localhost:5173"];
+app.use(
+    cors({
+        origin: function (origin, callback) {
+            if (!origin || allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("Not allowed by CORS"));
+            }
+        },
+        methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+        credentials: true,
+    })
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // 
 app.listen(3000, () => {
-  console.log("running ...");
+    console.log("running ...");
 });
-
-// // close the server
-app.get("/hi", function (req, res) {
-  res.send("hello");
+// 
+app.get("/", (req, res) => {
+    res.send("Home");
 });
-
+// server closing endpoint; no need what so ever
+app.get("/Hi", (req, res) => {
+    res.send("Hello");
+});
---
+++ indexx.js
@@ -0,0 +1,45 @@
+/* eslint-disable no-undef */
+const app = require("./src/app");
+const {initDB,mongodbConnection} = require("./src/config/mongodb");
+const config = require("./src/config");
+require("dotenv").config();
+
+/*
+async function bootstrap() {
+  const server = app.listen(config.port, async () => {
+    console.log(`Server running on port ${config.port}`);
+    // await mongodbConnection();
+  });
+
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        console.log("Server closed");
+      });
+    }
+    process.exit(1);
+  };
+
+  const unexpectedErrorHandler = (error) => {
+    console.log("unexpectedErrorHandler: " + error.message);
+    exitHandler();
+  };
+
+  process.on("uncaughtException", unexpectedErrorHandler);
+  process.on("unhandledRejection", unexpectedErrorHandler);
+}
+// bootstrap();
+*/
+
+// initialize the database
+initDB();
+// 
+app.listen(3000, () => {
+  console.log("running ...");
+});
+
+// // close the server
+app.get("/hi", function (req, res) {
+  res.send("hello");
+});
+
