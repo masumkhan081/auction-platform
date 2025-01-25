7c252cfc888f8ffeca8e93ad023d64999b29a54e
masumkhan081 <masumkhan081@gmail.com>
2025-01-25 07:47:53 +0600

    commited

--- index.js
+++ index.js
@@ -1,44 +1,24 @@
-/* eslint-disable no-undef */
-const app = require("./src/app");
-const { initDB } = require("./src/config/mongodb");
-const config = require("./src/config");
+const express = require("express");
+const app = express();
 require("dotenv").config();
-
-async function bootstrap() {
-  const server = app.listen(config.port, () => {
-    console.log(`Server running on port ${config.port}`);
-    initDB();
-  });
-
-  const exitHandler = () => {
-    if (server) {
-      server.close(() => {
-        console.log("Server closed");
-      });
-    }
-    process.exit(1);
-  };
-
-  const unexpectedErrorHandler = (error) => {
-    console.log("unexpectedErrorHandler: " + error.message);
-    exitHandler();
-  };
-
-  process.on("uncaughtException", unexpectedErrorHandler);
-  process.on("unhandledRejection", unexpectedErrorHandler);
-}
-bootstrap();
-
-
+const { initDB, mongodbConnection } = require("./src/config/mongodb");
+const originControl = require("./src/middlewares/corsMiddleware")
 // initialize the database
-// initDB();
-//
-// app.listen(3000, () => {
-//   console.log("running ...");
-// });
-
-// // close the server
-// app.get("/hi", function (req, res) {
-//   res.send("hello");
-// });
-
+// mongodbConnection();
+initDB();
+// middlewares
+// app.use(originControl);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// 
+app.listen(3000, () => {
+    console.log("running ...");
+});
+// 
+app.get("/", (req, res) => {
+    res.send("Home !!");
+});
+// server closing endpoint; no need what so ever
+app.get("/Hi", (req, res) => {
+    res.send("Hello");
+});
---
+++ indexx.js
@@ -0,0 +1,44 @@
+/* eslint-disable no-undef */
+const app = require("./src/app");
+const { initDB } = require("./src/config/mongodb");
+const config = require("./src/config");
+require("dotenv").config();
+
+async function bootstrap() {
+  const server = app.listen(config.port, () => {
+    console.log(`Server running on port ${config.port}`);
+    initDB();
+  });
+
+  const exitHandler = () => {
+    if (server) {
+      server.close(() => {
+        console.log("Server closed");
+      });
+    }
+    process.exit(1);
+  };
+
+  const unexpectedErrorHandler = (error) => {
+    console.log("unexpectedErrorHandler: " + error.message);
+    exitHandler();
+  };
+
+  process.on("uncaughtException", unexpectedErrorHandler);
+  process.on("unhandledRejection", unexpectedErrorHandler);
+}
+bootstrap();
+
+
+// initialize the database
+// initDB();
+//
+// app.listen(3000, () => {
+//   console.log("running ...");
+// });
+
+// // close the server
+// app.get("/hi", function (req, res) {
+//   res.send("hello");
+// });
+
