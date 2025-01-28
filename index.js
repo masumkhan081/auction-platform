3b818cd2c1993667c4561acef4eb8b0ddca891f1
masumkhan081 <masumkhan081@gmail.com>
2025-01-29 00:04:41 +0600

    `commited`

--- index.js
+++ index.js
@@ -1,33 +1,65 @@
-/* eslint-disable no-undef */
-const app = require("./src/app");
-const { initDB, mongodbConnection } = require("./src/config/mongodb");
-const config = require("./src/config");
+const express = require("express");
+const app = express();
 require("dotenv").config();
+// const RootRoutes = require("./src/root.route");
+const fs = require("fs");
+const path = require("path");
+const morgan = require("morgan");
+const winston = require("winston");
+const { initDB, mongodbConnection } = require("./src/config/mongodb");
+// const originControl = require("./src/middlewares/corsMiddleware")
+// 
+// initialize the database
+// initDB();
+// middlewares
+// app.use(originControl);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+// const publicDir = path.join(__dirname, "public");
+// just to ensure the public folder exists or create it.
+// if (!fs.existsSync(publicDir)) {
+//     fs.mkdirSync(publicDir, { recursive: true });
+//     console.log("Public folder created.");
+// }
+// app.use("/public", express.static("public"));
+// 
+// Configure Winston logger
+// const logger = winston.createLogger({
+//     level: "info",
+//     format: winston.format.combine(
+//         winston.format.timestamp(),
+//         winston.format.json()
+//     ),
+//     transports: [
+//         new winston.transports.Console(),
+//         new winston.transports.File({ filename: "combined.log" }),
+//     ],
+// });
 
-async function bootstrap() {
-    const server = app.listen(config.port, async () => {
-        console.log(`Server running on port ${config.port}`);
-        await mongodbConnection();
+// need to organize how message format looks like
+// app.use(
+//   morgan("combined", {
+//     stream: {
+//       write: (message) => logger.info(message.trim()),
+//     },
+//   })
+// );
+//
+app.get("/", (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "I am functional !",
+        data: null,
     });
+});
+// 
+// server closing endpoint; no need what so ever
+app.get("/Hi", (req, res) => {
+    res.send("Hello");
+});
+// 
+app.listen(3000, async () => {
+    console.log("running ...");
+    await mongodbConnection(); 
 
-    const exitHandler = () => {
-        if (server) {
-            server.close(() => {
-                console.log("Server closed");
-            });
-        }
-        process.exit(1);
-    };
-
-    const unexpectedErrorHandler = (error) => {
-        console.log(error);
-        exitHandler();
-    };
-
-    process.on("uncaughtException", unexpectedErrorHandler);
-    process.on("unhandledRejection", unexpectedErrorHandler);
-}
-
-bootstrap();
-
-
+});
