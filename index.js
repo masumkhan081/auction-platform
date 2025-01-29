9104528b4f23a6a3264a08d3417c6cbad1ef0eb8
masumkhan081 <masumkhan081@gmail.com>
2025-01-29 21:54:06 +0600

    `commited`

--- index.js
+++ index.js
@@ -7,10 +7,7 @@ const { initDB, mongodbConnection } = require("./src/config/mongodb");
 const originControl = require("./src/middlewares/corsMiddleware")
 const { morganMiddleware } = require("./src/config/logger");
 // --------------------------------------------------- Routes
-// const apiRoutes = require("./src/root.route.js");
-const categoryRoutes = require("./src/modules/category/category.route");
-// const productRoutes = require("./src/modules/product/product.route");
-const auctionRoutes = require("./src/modules/auction/auction.route");
+const apiRoutes = require("./src/root.route.js");
 //  -------------------------------------------------- Middlewares
 app.use(originControl);
 app.use(express.json());
@@ -25,9 +22,7 @@ if (!fs.existsSync(publicDir)) {
 app.use("/public", express.static("public"));
 
 // API Routes
-app.use("/api/categories", categoryRoutes);
-// app.use("/api/products", productRoutes);
-app.use("/api/auctions", auctionRoutes);
+app.use("/api", apiRoutes);
 
 // Deployment Check - Root Route
 app.get("/", (req, res) => {
